0f28d45f2973b84c4712e33a072caef9252427d3
marynolan6 <122495472+marynolan6@users.noreply.github.com>
2023-11-25 13:35:23 -0800

    Create Particles.cpp

---
+++ code/Particles.cpp
@@ -0,0 +1,54 @@
+#include "Particle.h"
+#include <cmath>
+#include <cstdlib>
+#include <SFML/Graphics/RenderWindow.hpp>
+
+/* generating a randomized shape with numPoints vertices, centered on mouseClickPosition 
+mapped to the Cartesian plane, which is centered at (0,0) - give the particle initial 
+vertical, horizontal, and angular / spin velocities */
+Particle::Particle(RenderTarget& target, int numPoints, Vector2i mouseClickPosition)
+{
+  
+}
+
+// overrides the virtual function from sf::Drawable to allow our draw function to polymorph
+void Particle::draw(RenderTarget& target, RenderStates states) const 
+{
+  
+}
+
+// updating the state of the particle
+void Particle::update(float dt) 
+{
+  
+}
+
+// rotation transformation to the particle's matrix of coordinates - specified by theta in radians
+void Particle::rotate(double theta) 
+{
+  
+}
+
+// scaling transformation to the particle's matrix of coordinates
+void Particle::scale(double c) 
+{
+  
+}
+
+// translation transformation to the particle's matrix of coordinates - moves horizonal/vertical
+void Particle::translate(double xShift, double yShift) 
+{
+  
+}
+
+// if 2 double-precision floating-point numbers are ~equal
+bool Particle::almostEqual(double a, double b, double eps) 
+{
+  
+}
+
+// rotations, scaling, and translations
+void Particle::unitTests() 
+{
+  
+}
