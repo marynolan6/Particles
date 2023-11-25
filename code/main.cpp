4521c4acb4f4df44c1814953020dce424b34fcb2
marynolan6 <122495472+marynolan6@users.noreply.github.com>
2023-11-25 12:17:08 -0800

    Create main.cpp

---
+++ code/main.cpp
@@ -0,0 +1,10 @@
+#include "Engine.h"
+int main()
+{
+	// Declare an instance of Engine
+	Engine engine;
+	// Start the engine
+	engine.run();
+	// Quit in the usual way when the engine is stopped
+	return 0;
+}
